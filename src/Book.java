313b23699b3648dedb4fd8fdbfda381c010defce
Aleksandr Gerb <aleksandrgerb87@gmail.com>
2022-08-28 21:58:19 +0300

    Старт ДЗ9

---
+++ src/Book.java
@@ -0,0 +1,33 @@
+public class Book {
+    String name;
+    String author;
+    int year;
+
+    public Book(String name, String author, int year) {
+        this.author = author;
+        this.year = year;
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+
+
+
+
+
+}
